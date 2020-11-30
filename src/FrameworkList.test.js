1714f4ec74432e1cf275917ec8fd26eedbcad142
Marie Otaki <otakimarie@gmail.com>
2020-11-30 14:50:49 -0800

    wtite a test to check if the list items are rendered correctly

--- src/FrameworkList.test.js
+++ src/FrameworkList.test.js
@@ -9,4 +9,20 @@ describe('Rendering the list with props', () => {
     render(<FrameworkList />);
     expect(screen.getByText('No data!')).toBeInTheDocument();
   });
+
+  it('Should render list item correctly', () => {
+    const dammyData = [
+      { id: 1, item: 'React dummy' },
+      { id: 2, item: 'Angular dummy' },
+      { id: 3, item: 'Vue dummy' },
+    ];
+
+    render(<FrameworkList frameworks={dammyData} />);
+    const frameworkItems = screen
+      .getAllByRole('listitem')
+      .map((elm) => elm.textContent);
+    const dummyItems = dammyData.map((elm) => elm.item);
+    expect(frameworkItems).toEqual(dummyItems);
+    expect(screen.queryByText('No data!')).toBeNull();
+  });
 });
